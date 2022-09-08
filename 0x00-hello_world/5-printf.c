8b5b7cfee890ac0af503ebfad68b74145d611b0e
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-08 08:56:52 -0700

    fifth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-  int main(void)
+int main(void)
 {
-  	printf("with proper grammar, but the outcome is a piece of art,\n");
-	Return(0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    Return(0);
 }
