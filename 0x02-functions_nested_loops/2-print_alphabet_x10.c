4eadf8835ff2b20ec2893f9faf9e4ed50c57fe7e
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-14 05:58:29 -0700

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#incude "main.h"
+/**
+ * main - Entry
+ *
+ * Return: 0 always success
+ void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n')
+}
