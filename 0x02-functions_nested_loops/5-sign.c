583ffc09f7a47c440318ba2a0905311ac0d8e87c
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-14 11:54:21 -0700

    task 6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,26 +13,16 @@ int print_sign(int n)
 	{
 	_putchar('+');
 	return (1);
-
-}
+	}		
 
 	else if (n < 0)
-	
 	{
-
 	_putchar('-')
-
 	return (-1);
-
 	}
-
 	else
-
 	{
-
 	_putchar('0');
-
 	return (0);
-
 	}
 }
