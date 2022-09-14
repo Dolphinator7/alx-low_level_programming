628de4afb85fb452c7821bc901f58e39fd79c481
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-14 00:35:57 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,15 +10,17 @@
  */
 int main(void)
 {
-	_putchar(';');
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int _putchar
+		{
+			_putchar('_');
+			_putchar('p');
+			_putchar('u');
+			_putchar('t');
+			_putchar('c');
+			_putchar('h');
+			_putchar('a');
+			_putchar('r');
+		}
 
 	_putchar('\n');
 
