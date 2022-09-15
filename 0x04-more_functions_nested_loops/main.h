a2b610bb6f6a7bf51e1698c8e483fd871a9b99dc
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-15 05:44:52 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,18 +1,13 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
 
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -24,4 +19,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 
-#endif /* MAIN.H */
+#endif /* MAIN_H */
