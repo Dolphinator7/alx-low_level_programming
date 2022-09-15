635b77121d4045d9bbdba76ca84dde5e88bbef25
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-15 00:56:10 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int n;
 
-	if (last_digit < 10)
-		last_digit *= -1;
-
-	_putchar(last_digit + '0');
-
-	return (last_digit + '0');
+	print_last_digit(98);
+	print_last_digit(0);
+	n = print_last_digit(-1024);
+	_putchar('0' + n);
+	_putchar('\n');
+	return (0);
 }
