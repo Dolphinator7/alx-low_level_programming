8a21b534c1df761759529a4e2556e2f9ab235169
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-20 06:36:26 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ *@str: string being tested
+ *
+ */
+
+void puts_half(char *str)
+{
+	int ct, even, neg;
+
+	for (ct = 0; str[ct] != '\0'; ct++)
+		;
+	if ((ct % 2) == 0)
+	{
+		for (even = ct / 2; str[even] != '\0'; even++)
+			_putchar(str[even]);
+	}
+	else
+	{
+		for (neg = (ct - 1) / 2; str[neg] != '\0'; neg++)
+			_putchar(str[neg + 1]);
+	}
+	_putchar('\n');
+}
