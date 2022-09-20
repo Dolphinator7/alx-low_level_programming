9af98064012aec7e83ea9d2363015f053f801d22
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-20 05:49:19 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,9 @@
 
 void print_rev(char *s)
 {
-	int i, c = 0;
+	int len = strlen(s);
 
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		c++;
-	}
-	for (i = c - 1; i >= 0; i--)
-	{
-		_putchar(*(s + i));
-	}
-	_putchar('\n');
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
 }
