092da30bbfc9a60324a21ec31ceb07000a933eb1
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-20 02:27:14 -0700

    task

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifindef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototypes()
+ * int protptypes()
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strien(char *s);
+void _puts(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
+
+#endif /*MAIN_H*/
