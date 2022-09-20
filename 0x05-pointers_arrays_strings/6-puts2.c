f1b1be5f874d9ee43cd4ab2be54399ca15afa27e
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-20 06:20:43 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string
+ *@str: char being printed
+ */
+
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if ((a % 2) == 0)
+			_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
