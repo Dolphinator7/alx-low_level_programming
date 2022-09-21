10b7c87a8c40b48a3f878a37bb41b744c9337d64
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-21 01:06:45 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,15 +5,16 @@
  * puts_half - prints half of the string
  *@str: string being tested
  *
- * return: void
+ * return: the len of str
  */
 
 void puts_half(char *str)
 {
 	int len = 0;
 
-	while (s[len] / 2 != '\0')
+	while (str[len] / 2 != '\0')
 	{
 		len++
 	}
+	return (len);
 }
