226793eaac9a8a26a84d1e3cc2c971e4f4a453fb
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-22 00:19:10 -0700

    hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,16 @@
 #include "main.h"
+#include <string>
 
 /**
- * _strncat - will use at most n bytes from src
- * does not need to be null-terminated if it contains n or more bytes
- *@dest: buffer storing the string copy
- @src: the source string
- @n: max number of bytes copied
- * return: return a pointer to the resulting string dest
+ * _strncat - function to append some charx
+ *@dest: first param
+ @src: second param
+ @n: third param
+ * return: string
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	strncat (dest, src, n);
 	return (dest);
-
 }
