f14ad6808e694a5547ae90a3632074d9ab816a38
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-22 00:48:28 -0700

    hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,14 +3,14 @@
 
 /**
  * _strncat - function to append some charx
- *@dest: first param
- @src: second param
- @n: third param
- * return: string
+ * @dest: first param
+ * @src: second param
+ * @n: 3rd param
+ * Return: string
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat (dest, src, n);
+	strncat(dest, src, n);
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,25 +2,15 @@
 #include <string.h>
 
 /**
- * _strncpy - c function that copies a string, including the
- * terminating null byte
- * the length of the source string
- * works identically to the standard library function strncpy
- * @dest: buffer storing the string copy
- * @src: the source string
- * @n: max number of byte copied
- * return: returns
+ * _strncpy - for copying purpose
+ * @dest: param1
+ * @src: param2
+ * @n: param3
+ * return: string
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-
+	strncpy(dest, src, n);
 	return (dest);
 }
