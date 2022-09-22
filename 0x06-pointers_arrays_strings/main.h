1286048295b05409a587e9c71a8287a01a4c7800
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-09-22 05:50:45 -0700

    hot fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -18,5 +18,6 @@ char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
+void print_buffer(char *b, int size);
 
 #endif /* MAIN_H */
