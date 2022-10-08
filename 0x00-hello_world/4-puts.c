eef3f5638ad39ae8f5f3a5760b43649381084616
“Dolphinator7” <petersideudogwu7@gmail.com>
2022-10-08 01:16:42 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,2 @@
-#include <stdio.h>
-/**
- * main - entry point
- *
- * Return: 0 after printing the function
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+#!/bin/bash
+gcc $CFILE -o cisfun
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - Program that prints
+ *
+ * Return: always 0
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
